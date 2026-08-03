61aff2a5c9009fbb6af6938f7273c9df6f2b423d
Hwang_Junhyun <h010617@naver.com>
2026-08-03 16:17:54 +0900

    feat : RPC추가

--- CH_4/Source/CH_4/Player/NBPlayerController.h
+++ CH_4/Source/CH_4/Player/NBPlayerController.h
@@ -11,12 +11,24 @@ class CH_4_API ANBPlayerController : public APlayerController
 {
     GENERATED_BODY()
 
+public:
+    void SubmitGuess(const FString& Guess);
+
+    UFUNCTION(Client, Reliable)
+    void ClientRPCReceiveMessage(const FString& Message);
+
 protected:
     virtual void BeginPlay() override;
+
+    UFUNCTION(Server, Reliable)
+    void ServerRPCSubmitGuess(const FString& Guess);
 
     UPROPERTY(EditDefaultsOnly, Category = "UI")
     TSubclassOf<UUserWidget> MainWidgetClass;
 
     UPROPERTY()
     TObjectPtr<UUserWidget> MainWidget;
+
+private:
+    void PrintMessage(const FString& Message) const;
 };
